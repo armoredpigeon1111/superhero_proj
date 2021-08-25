1f9154d1ba20ce0994cd1d05ab92ab6b264e0dc9
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-25 14:37:00 -0400

    changed path parameters

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Superhero
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!")
+    all_heroes = Superhero.objects.all()
+    return render(request, 'superheroes/index.html')
