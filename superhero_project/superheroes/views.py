b46604e49b4eb24084a0a5b5dca0d0107990aef1
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-25 14:46:16 -0400

    Added context for all_heroes

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Superhero
 
 
 # Create your views here.
 def index(request):
     all_heroes = Superhero.objects.all()
-    return render(request, 'superheroes/index.html')
+    context = {
+        'all_heroes': all_heroes
+    }
+    return render(request, 'superheroes/index.html', context)
+    
